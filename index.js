7fca96a1b894c9865a6bf0e2b1cbc1330fbe6e42
Benjamin Lee <nycxbl@hotmail.com>
2018-07-16 04:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ function removeFromCart(item) {
   return getCart().splice(itemIndex, 1);
 }
 
-function placeOrder(card){
-  if(card === null){
+function placeOrder(card=0){
+  if(card === 0){
     console.log("Sorry, we don't have a credit card on file for you..");
   } else {
     var sum = total();
