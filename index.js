df6113820c97d3c2b0862de9710fa6ce2b3ab815
Benjamin Lee <nycxbl@hotmail.com>
2018-07-16 03:27:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,27 @@ function setCart(c) {
 function addToCart(item){
   var newItem = generateCartItem(item);
   getCart().push(newItem);
+  return `s{newItem.itemName} has been added to your cart.`;
 }
-
+function viewCart() {
+  var description = "In your cart, you have ";
+  for (let i = 0; i < cart.length; i++){
+    description = description + `${cart[i].itemName} at $${cart[i].itemPrice}`;
+    if (i === cart.length){
+      description = description + ",";
+    }
+  }
+  return `${description}.`;
+}
+//helper
 function generateCartItem(item){
   var newItem = {
-    item: item,
-    price: Math.floor(Math.random()*100)
-  }
+    itemName: item,
+    itemPrice: getRandomInt(1, 100),
+  };
+  
   return newItem;
+}
+function getRandomInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
