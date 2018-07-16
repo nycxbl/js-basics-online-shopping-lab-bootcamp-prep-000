c3d3c681e1db2ca09b654e07d5207182f34ca729
Benjamin Lee <nycxbl@hotmail.com>
2018-07-16 03:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function viewCart() {
   }
   var description = "In your cart, you have ";
   for (let i = 0; i < cart.length; i++){
-    description = `${description} ${cart[i].itemName} at $${cart[i].itemPrice}`;
+    description = `${description}${cart[i].itemName} at $${cart[i].itemPrice}`;
     if (i >= 0 && i < cart.length-2) {
       description = `${description}, `;
     }
