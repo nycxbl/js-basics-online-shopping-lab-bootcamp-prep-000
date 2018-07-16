3834d93bf67f56cf6c893c5448ed36e1101db4f1
Benjamin Lee <nycxbl@hotmail.com>
2018-07-16 03:43:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,12 +21,13 @@ function viewCart() {
   var description = "In your cart, you have ";
   for (let i = 0; i < cart.length; i++){
     description = description + `${cart[i].itemName} at $${cart[i].itemPrice}`;
+        if (i > 0 && i < cart.length-2){
+      description = `${description}, `;
+    }
     if (i === cart.length-2){
       description = `${description}, and `;
     }
-    if (i > 0 && i < cart.length-2){
-      description = `${description}, `;
-    }
+
     
   }
   return `${description}.`;
